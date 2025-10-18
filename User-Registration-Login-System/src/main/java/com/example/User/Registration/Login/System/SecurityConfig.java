6243ebc0e8bc13039cfd9e439652e8ba00874f4d
Brayan Rivas <164957185+bgrivas01@users.noreply.github.com>
2025-10-18 16:13:01 -0500

    Enhance SecurityConfig with explanatory comments
    Added comments to explain the purpose of the SecurityConfig class and its methods.

--- User-Registration-Login-System/src/main/java/com/example/User/Registration/Login/System/SecurityConfig.java
+++ User-Registration-Login-System/src/main/java/com/example/User/Registration/Login/System/SecurityConfig.java
@@ -9,24 +9,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@RequiredArgsConstructor
+@Configuration //marks a class as a source of bean definitions
+@RequiredArgsConstructor //creates contructor for all my "final feilds"
 public class SecurityConfig {
 
-    private final PasswordEncoder passwordEncoder;
+    //PasswordEncoder is a interface used to hash passwords
+    private final PasswordEncoder passwordEncoder; //since we have @RequiredArgsContructor we dont need to make a contructor for this
 
+    //code below defines all security rules
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { 
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/auth/**").permitAll()
-                    .anyRequest().authenticated()
+            .csrf(csrf -> csrf.disable()) //this disables csrf(Cross-Sight-Request-Forgery) because we will use tokens to protect us unstead 
+            .authorizeHttpRequests(auth -> auth  //with this and the following code we will decide who can see what
+                    .requestMatchers("/api/auth/**").permitAll() //this says that any request that contains "/api/auth/" is open to anyone to see
+                    .anyRequest().authenticated() //this says that if you want to see anything else you have to be authorized(logged in) to do so
             )
-            .formLogin(login -> login.permitAll());
-        return http.build();
+            .formLogin(login -> login.permitAll());  //this means anyone can use the login form that springSecurity provides
+        return http.build();  //builds our securityConfig and gives it to spring during runtime
     }
 
+    //the code below is what is responisble for authenticating users(verifying there credentials)
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
